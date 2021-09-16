62e5a176dcb00749fe9e3c2361dcc2666bdac666
jingliuweb@gmail.com <jingliuweb@gmail.com>
2021-09-16 13:06:45 +0200

    Finish ex03

---
+++ Ex03/Ex03/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ConcatenateString("Hello", "World"));
+        }
+
+        private static string ConcatenateString(string v1, string v2)
+        {
+            return string.Format("{0} {1}", v1, v2);
+        }
+    }
+}
