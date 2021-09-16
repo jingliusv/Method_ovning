80787d456f2497090d9d7faba98049451af48e2e
jingliuweb@gmail.com <jingliuweb@gmail.com>
2021-09-16 12:59:26 +0200

    Finish ex02

---
+++ Ex02/Ex02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Mata in ett decimaltal mellan 1 och 0:");
+            double input = Convert.ToDouble(Console.ReadLine());
+            ToPercentage(input);
+        }
+
+        private static void ToPercentage(double input)
+        {
+            Console.WriteLine(input * 100 + "%");
+        }
+    }
+}
