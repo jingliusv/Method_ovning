eae5c49a179f94fcd5554168ce8a6dbd15639d53
jingliuweb@gmail.com <jingliuweb@gmail.com>
2021-09-16 14:16:15 +0200

    finish ex07

---
+++ Ex07/Ex07/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = {"stockholm", "kamera", "programmering", "dator" };
+            HittaLangstaOrdet(words);
+        }
+
+        public static void HittaLangstaOrdet(string[] words)
+        {
+            int wordLength = 0;
+            string longestWord = string.Empty;
+            foreach (string word in words)
+            {
+                if(word.Length > wordLength)
+                {
+                    wordLength = word.Length;
+                    longestWord = word;
+                }                   
+            }
+            Console.WriteLine(longestWord);
+        }
+    }
+}
