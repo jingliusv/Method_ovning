9b84d3e89574d2211bcd9e20633eed10fc89e212
jingliuweb@gmail.com <jingliuweb@gmail.com>
2021-09-16 14:44:58 +0200

    finish ex09

---
+++ Ex09/Ex09/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Mata in en bokstav: ");
+            string input = Console.ReadLine();
+            if(CheckVokal(input))
+                Console.WriteLine("Det är en vokal.");
+            else
+                Console.WriteLine("Det är inte en vokal.");
+        }
+
+        private static bool CheckVokal(string input)
+        {
+            bool isVokal = false;
+            string[] vokal = { "a", "e", "i", "o", "u", "y", "å", "ä", "ö" };
+            foreach(var letter in vokal)
+            {
+                if (letter.Equals(input))
+                {
+                    isVokal = true;
+                    break;
+                }              
+                else
+                    isVokal = false;
+            }
+            return isVokal;
+        }
+    }
+}
