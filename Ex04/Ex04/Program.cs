26b5413bbdac8383bf31ce1ce8c3cf953d68777c
jingliuweb@gmail.com <jingliuweb@gmail.com>
2021-09-16 13:14:19 +0200

    Finish ex04

---
+++ Ex04/Ex04/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CaculateTax(250)); ;
+        }
+
+        public static int CaculateTax(double input)
+        {
+            const double taxRate = 0.25;
+            return Convert.ToInt32(input * taxRate);
+        }
+    }
+}
